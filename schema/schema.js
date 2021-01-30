824ba1292534ece4e8809e37a49d0ad3d5fb2fda
Ray Chung <ray.j.chung@gmail.com>
2021-01-30 02:25:25 -0800

    Add schema; add test api with json-server.

---
+++ schema/schema.js
@@ -0,0 +1,32 @@
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = require('graphql');
+const axios = require('axios');
+
+
+const UserType = new GraphQLObjectType({
+  name: 'User',
+  fields: {
+    id: { type: GraphQLString },
+    firstName: { type: GraphQLString },
+    age: { type: GraphQLInt },
+  }
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      // return type
+      type: UserType,
+      // input args
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return axios.get(`http://localhost:3000/users/${args.id}`)
+          .then(resp => resp.data);
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+});
