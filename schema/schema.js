388b1b97cf90f9f8974f8a75e36c5d1a4f488392
Ray Chung <ray.j.chung@gmail.com>
2021-01-30 23:42:02 -0800

    Add company schema.

--- schema/schema.js
+++ schema/schema.js
@@ -1,6 +1,14 @@
 const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = require('graphql');
 const axios = require('axios');
 
+const CompanyType = new GraphQLObjectType({
+  name: 'Company',
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+  }
+});
 
 const UserType = new GraphQLObjectType({
   name: 'User',
@@ -8,6 +16,14 @@ const UserType = new GraphQLObjectType({
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
+    company: {
+      type: CompanyType,
+      resolve(parentValue, args) {
+        const { companyId } = parentValue;
+        return axios.get(`http://localhost:3000/companies/${companyId}`)
+          .then(resp => resp.data);
+      }
+    },
   }
 });
 
